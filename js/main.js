d725568bc17f4e6db600df4ab078f4500a531c1f
ChadCooper06 <chadcooper06@gmail.com>
2022-02-10 04:44:44 +0000

    <added first JS>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 
+function updateTime() {
+    const date = new date;
+}
+
+const hour = formatTime(date.getHours());
+const minutes = formatTime(date.getMinutes());
+const seconds = formatTime(date.getSeconds());
+
+function formatTime(time) {
+    if (time < 10) {
+        return 0 + time;
+    }
+    return time;
+} 
+
+function setAlarm(value) {
+    setAlarm = value;
+}
+
+setInterval(updateTime, 1000);
