7a95dd26058e5114dd433d543fe530751033134e
ChadCooper06 <chadcooper06@gmail.com>
2022-02-10 19:08:56 +0000

    <think I fixed it>

--- js/main.js
+++ js/main.js
@@ -4,28 +4,27 @@ function updateTime() {
 
     let date = new Date ();
 
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const hours = formatTime(date.getHours());
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
 
     const period = "AM";
-    if (hour == 0) hour = 12;
+    if (hours == 0) hours = 12;
     
-    if (hour > 12) {
-        hour = hour - 12;
+    if (hours > 12) {
+        hours = hours - 12;
         period = "PM";
     }
 
-    display.innerText = `${hour} : ${minutes} : ${seconds} : ${period}`;
+    display.innerText = `${hours} : ${minutes} : ${seconds} : ${period}`;
     
     setInterval(updateTime, 1000);
 
-};
-
+}
 
 function formatTime(time) {
     if (time < 10) {
-        return 0 + time;
+        return '0' + time;
     }
     return time;
 }; 
